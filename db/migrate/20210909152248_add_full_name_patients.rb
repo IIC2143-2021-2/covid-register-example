e7ecda8a973dc6785207d3ba3cbb54ba2814eff6
Macarena Peralta <mperalta.costoya@gmail.com>
2021-09-30 12:18:47 -0300

    add seed

--- db/migrate/20210909152248_add_full_name_patients.rb
+++
@@ -1,4 +0,0 @@
-class AddFullNamePatients < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Destroying existing records'
+Patient.destroy_all
+puts 'Creating patients'
+10.times do
+  Patient.create( name: Faker::Name.first_name, last_name: Faker::Name.last_name, run: Faker::ChileRut.unique.full_rut, birth_date: Faker::Date.between(from: 100.years.ago, to: 12.years.ago) ) 
+end
+
+
+Patient.create(name: 'Elizabeth', last_name: 'Swan', run: Faker::ChileRut.unique.full_rut, birth_date: Faker::Date.between(from: 80.years.ago, to: 18.years.ago))
+Patient.create(name: 'Elizabeth', last_name: 'Bennet', run: Faker::ChileRut.unique.full_rut, birth_date: Faker::Date.between(from: 80.years.ago, to: 18.years.ago))
+Patient.create(name: 'Elizabeth', last_name: 'Olsen', run: Faker::ChileRut.unique.full_rut, birth_date: Faker::Date.between(from: 80.years.ago, to: 18.years.ago))
+Patient.create(name: 'Elizabeth', last_name: 'Windsort', run: Faker::ChileRut.unique.full_rut, birth_date: 95.years.ago)
+
+puts 'seed run successfully'
