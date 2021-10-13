15306d4123bcb948c56e4559a1cdfa783800e377
Macarena Peralta <mperalta.costoya@gmail.com>
2021-10-13 16:55:15 -0300

    add validations and callbacks to patients

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,21 @@
 class Patient < ApplicationRecord
+  before_save :my_method
+  validates :run, uniqueness: true
+  validates :name, :last_name, :birth_date, :run, presence: true
+  validates :name, :last_name, length: { minimum: 2 }
+  validates :run, format: { with: /\d{1,3}(?:\d{1,3}){2}-[\dkK]/, message: 'invalid, please use dots and hyphen, example: 12.345.678-k' }
+  validate :under_age_cannot_be_vaccinated
+  
+  def under_age_cannot_be_vaccinated
+    if birth_date.present? && birth_date > 12.years.ago
+      errors.add(:birth_date, 'No under 12 years old can be vaccinated')
+    end
+  end
+
+  def my_method
+    p "this method runs before save for patient #{name}"
+  end
+
   def full_name
     "#{name} #{last_name}"
   end
