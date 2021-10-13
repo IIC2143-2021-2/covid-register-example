5f15aa0a0289668d31213d40566edb121083f106
Macarena Peralta <mperalta.costoya@gmail.com>
2021-10-13 16:54:49 -0300

    add test and factory bot

---
+++ spec/models/patient_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Patient, type: :model do
+  context 'when accessing a Patient' do
+
+    let(:patient) { create(:patient) }
+
+    describe 'full_name method' do
+      it 'returns the name' do
+        expect(patient.full_name).to(include(patient.name))
+      end
+
+      it 'returns the last name' do
+        expect(patient.full_name).to(include(patient.last_name))
+      end
+    end  
+  end
+end
